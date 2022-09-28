0c0a9cbea5102f9e70a360f2138fa8bcb307fab8
onadan <onadan45@gmail.com>
2022-09-28 11:34:18 +0100

    added pwa

---
+++ serviceWorker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "version-1";
+
+const urlsToCache = [
+  "/index.html",
+  "/src/css/main.css",
+  "/src/js/app.js",
+  "src/assets/icons/cube.svg",
+  "src/assets/images/mee.png",
+];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
