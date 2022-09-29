302d353c6f9d76b12e1338a77c15e4d2725aef78
onadan <onadan45@gmail.com>
2022-09-29 09:23:49 +0100

    added parallax effect

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,31 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .catch((err) => console.log("Failure", err));
-  });
+//Let Go
+
+//service worker
+
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/serviceWorker.js")
+//       .catch((err) => console.log("Failure", err));
+//   });
+// }
+
+
+//parallax
+document.addEventListener('mousemove', parallax);
+function parallax(e){
+  this.querySelectorAll('.layer').forEach(layer => {
+    const speed = layer.getAttribute('data-speed')
+
+    const x = (window.innerWidth - e.pageX*speed)/100
+    const y = (window.innerWidth - e.pageY*speed)/80
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+  })
 }
+
+
+//others
 
 const body = document.querySelector("body");
 
