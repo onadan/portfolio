86a5afdac44f5460d7bd734ab8330d0db17f922c
onadan <onadan45@gmail.com>
2022-09-29 09:24:47 +0100

    added parallax effect

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@
 
 //service worker
 
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("/serviceWorker.js")
-//       .catch((err) => console.log("Failure", err));
-//   });
-// }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .catch((err) => console.log("Failure", err));
+  });
+}
 
 
 //parallax
