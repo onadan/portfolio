2fb0cac41562a41101dfee3585ff37c702b63ed0
onadan <onadan45@gmail.com>
2022-09-29 09:31:54 +0100

    added parallax effect

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,14 +4,26 @@ const urlsToCache = [
   "/index.html",
   "/src/css/main.css",
   "/src/js/app.js",
-  "src/assets/icons/cube.svg",
-  "src/assets/images/mee.png",
+  "/src/assets/icons/cube.svg",
+  "/src/assets/images/mee.png",
+  "/src/assets/elements/1.png",
+  "/src/assets/elements/2.png",
+  "/src/assets/elements/3.png",
+  "/src/assets/elements/4.png",
+  "/src/assets/elements/5.png",
+  "/src/assets/elements/6.png",
+  "/src/assets/elements/7.png",
+  "/src/assets/elements/8.png",
+  "/src/assets/elements/9.png",
+  "/src/assets/elements/10.png",
+  "/src/assets/elements/11.png",
+  "/src/assets/elements/12.png",
 ];
 
 self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
+      // console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
